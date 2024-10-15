2b818145647cdff5042d5f83cf487dd0e9aee22e
Juan Andrés Losauro <87399338+Talkird@users.noreply.github.com>
2024-10-15 12:44:40 -0300

    svelte link

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -5,6 +5,8 @@
 <footer
   class="bg-neutral text-light font-bold items-center justify-center gap-1 p-4 flex"
 >
-  Made with <span class="text-accent font-extrabold"> Svelte </span> by Juan
-  Andrés Losauro <Heart size={16} />
+  Made with <a href="https://svelte.dev/" class="text-accent font-extrabold">
+    Svelte
+  </a>
+  by Juan Andrés Losauro <Heart size={16} />
 </footer>
